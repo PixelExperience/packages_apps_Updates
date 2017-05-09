ea85b164d7d1ff3efd5ade114119fe34b33447ec
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-05-08 21:00:52 -0300

    Changes for PureNexus
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- Android.mk
+++ Android.mk
@@ -12,8 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v7-cardview \
     android-support-v7-preference \
-    android-support-v7-recyclerview \
-    org.cyanogenmod.platform.internal \
+    android-support-v7-recyclerview
     volley
 
 LOCAL_RESOURCE_DIR := \
@@ -32,9 +31,7 @@ LOCAL_AAPT_FLAGS := \
     --extra-packages android.support.v7.recyclerview \
     --extra-packages android.support.v7.cardview
 
-LOCAL_AAPT_FLAGS += --rename-manifest-package org.lineageos.updater
-
-LOCAL_PACKAGE_NAME := CMUpdater
+LOCAL_PACKAGE_NAME := PureOTA
 
 LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
 
---
+++ src/com/purenexus/ota/receiver/DownloadReceiver.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.purenexus.ota.receiver;
+
+import android.app.DownloadManager;
+import android.app.StatusBarManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.purenexus.ota.R;
+import com.purenexus.ota.misc.Constants;
+import com.purenexus.ota.misc.UpdateInfo;
+import com.purenexus.ota.service.DownloadCompleteIntentService;
+import com.purenexus.ota.service.DownloadService;
+import com.purenexus.ota.utils.Utils;
+
+import java.io.IOException;
+
+public class DownloadReceiver extends BroadcastReceiver{
+    private static final String TAG = "DownloadReceiver";
+
+    public static final String ACTION_START_DOWNLOAD = "com.purenexus.ota.action.START_DOWNLOAD";
+    public static final String EXTRA_UPDATE_INFO = "update_info";
+
+    public static final String ACTION_DOWNLOAD_STARTED = "com.purenexus.ota.action.DOWNLOAD_STARTED";
+
+    static final String ACTION_INSTALL_UPDATE = "com.purenexus.ota.action.INSTALL_UPDATE";
+    static final String EXTRA_FILENAME = "filename";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (ACTION_START_DOWNLOAD.equals(action)) {
+            UpdateInfo ui = (UpdateInfo) intent.getParcelableExtra(EXTRA_UPDATE_INFO);
+            handleStartDownload(context, ui);
+        } else if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            handleDownloadComplete(context, id);
+        } else if (ACTION_INSTALL_UPDATE.equals(action)) {
+            StatusBarManager sb = (StatusBarManager) context.getSystemService(Context.STATUS_BAR_SERVICE);
+            sb.collapsePanels();
+            String fileName = intent.getStringExtra(EXTRA_FILENAME);
+            try {
+                Utils.triggerUpdate(context, fileName);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to reboot into recovery mode", e);
+                Toast.makeText(context, R.string.apply_unable_to_reboot_toast,
+                            Toast.LENGTH_SHORT).show();
+                Utils.cancelNotification(context);
+            }
+        }
+    }
+
+    private void handleStartDownload(Context context, UpdateInfo ui) {
+        DownloadService.start(context, ui);
+    }
+
+    private void handleDownloadComplete(Context context, long id) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        long enqueued = prefs.getLong(Constants.DOWNLOAD_ID, -1);
+        String fileName = prefs.getString(Constants.DOWNLOAD_NAME, null);
+        if (enqueued < 0 || id < 0 || id != enqueued || fileName == null) {
+            return;
+        }
+
+        // Send off to DownloadCompleteIntentService
+        Intent intent = new Intent(context, DownloadCompleteIntentService.class);
+        intent.putExtra(Constants.DOWNLOAD_ID, id);
+        intent.putExtra(Constants.DOWNLOAD_NAME, fileName);
+        context.startService(intent);
+
+        // Clear the shared prefs
+        prefs.edit()
+                .remove(Constants.DOWNLOAD_ID)
+                .remove(Constants.DOWNLOAD_NAME)
+                .apply();
+    }
+}
