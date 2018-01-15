753740ff822148a796ff3ca95cbc849c78b74764
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-15 01:52:44 -0200

    Changes for inline building
    * Import ExpandableLayout library from https://github.com/cachapa/ExpandableLayout
    * Cleanup mkfile
    * Import setupwizard for buttons

--- Android.mk
+++ Android.mk
@@ -6,38 +6,31 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_CERTIFICATE := platform
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-design \
-    android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-cardview \
-    android-support-v7-preference \
-    android-support-v14-preference \
-    android-support-v7-recyclerview \
+    android-support-design \
+    android-support-annotations \
+    android-support-v4 \
     volley \
     commons-io-2.6
 
 LOCAL_RESOURCE_DIR := \
-    $(TOP)/frameworks/support/design/res \
     $(TOP)/frameworks/support/v7/appcompat/res \
     $(TOP)/frameworks/support/v7/cardview/res \
-    $(TOP)/frameworks/support/v7/preference/res \
-    $(TOP)/frameworks/support/v14/preference/res \
-    $(TOP)/frameworks/support/v7/recyclerview/res \
-    $(LOCAL_PATH)/res
+    $(TOP)/frameworks/support/design/res \
+    $(LOCAL_PATH)/app/src/main/res
 
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.design \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v14.preference \
-    --extra-packages android.support.v7.recyclerview \
-    --extra-packages android.support.v7.cardview
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat:android.support.v7.cardview:android.support.design
 
-LOCAL_PACKAGE_NAME := PureOTA
+include frameworks/opt/setupwizard/library/common-gingerbread.mk
+
+LOCAL_PACKAGE_NAME := Updates
 
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_ENABLED := disabled
