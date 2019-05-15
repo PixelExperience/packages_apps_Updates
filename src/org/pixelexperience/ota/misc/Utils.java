2f0f35963cf24610c123d60a8641023ea43d56ec
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-05-15 10:27:15 -0300

    Cleanup stored zips if no updates available
    Change-Id: I6cd66f70d2dd3d4219cca78c030c4ddad16f3948

--- src/org/pixelexperience/ota/misc/Utils.java
+++ src/org/pixelexperience/ota/misc/Utils.java
@@ -245,11 +245,6 @@ public class Utils {
             }
         }
 
-        final String DOWNLOADS_CLEANUP_DONE = "cleanup_done";
-        if (preferences.getBoolean(DOWNLOADS_CLEANUP_DONE, false)) {
-            return;
-        }
-
         Log.d(TAG, "Cleaning " + downloadPath);
         if (!downloadPath.isDirectory()) {
             return;
@@ -263,7 +258,9 @@ public class Utils {
         UpdatesDbHelper dbHelper = new UpdatesDbHelper(context);
         List<String> knownPaths = new ArrayList<>();
         for (UpdateInfo update : dbHelper.getUpdates()) {
-            knownPaths.add(update.getFile().getAbsolutePath());
+            if (isCompatible(update)){
+                knownPaths.add(update.getFile().getAbsolutePath());
+            }
         }
         for (File file : files) {
             if (!knownPaths.contains(file.getAbsolutePath())) {
@@ -271,8 +268,6 @@ public class Utils {
                 file.delete();
             }
         }
-
-        preferences.edit().putBoolean(DOWNLOADS_CLEANUP_DONE, true).apply();
     }
 
     public static File appendSequentialNumber(final File file) {
