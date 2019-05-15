a4aff30d2e9ce05910eee43404ca7c8779f6c23b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-05-15 10:11:59 -0300

    Utils: Match file size function from website
    Change-Id: I40ac6c603265a06ce2c873cb7b2019e5305bca81

--- src/org/pixelexperience/ota/misc/Utils.java
+++ src/org/pixelexperience/ota/misc/Utils.java
@@ -16,6 +16,7 @@
  */
 package org.pixelexperience.ota.misc;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -53,6 +54,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Locale;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -390,10 +392,17 @@ public class Utils {
         }
     }
 
+    @SuppressLint("DefaultLocale")
     public static String readableFileSize(long size) {
-        if(size <= 0) return "0";
-        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
-        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
-        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+        String units[] = new String[]{"B", "kB", "MB", "GB", "TB", "PB"};
+        int mod = 1024;
+        double power = (size > 0) ? Math.floor(Math.log(size) / Math.log(mod)) : 0;
+        String unit = units[(int)power];
+        double result = size / Math.pow(mod, power);
+        if (unit.equals("B")|| unit.equals("kB") || unit.equals("MB")){
+            result = (int)result;
+            return String.format("%d %s", (int)result, unit);
+        }
+        return String.format("%01.2f %s", result, unit);
     }
 }
