3bfcb84c6fb07b3bb29fba65ea41b2a2016f7cfb
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-03 21:06:52 -0300

    Fix update check on boot
    Change-Id: Ied741fb1ce0e1afdbfb689a2fde84411898288d0

--- src/org/pixelexperience/ota/UpdatesActivity.java
+++ src/org/pixelexperience/ota/UpdatesActivity.java
@@ -248,7 +248,7 @@ public class UpdatesActivity extends UpdatesListActivity {
     private void processNewJson(File json, File jsonNew, boolean manualRefresh) {
         try {
             loadUpdatesList(jsonNew, manualRefresh);
-            if (json.exists() && Utils.checkForNewUpdates(json, jsonNew)) {
+            if (json.exists() && Utils.checkForNewUpdates(json, jsonNew, false)) {
                 UpdatesCheckReceiver.updateRepeatingUpdatesCheck(this);
             }
             // In case we set a one-shot check because of a previous failure
