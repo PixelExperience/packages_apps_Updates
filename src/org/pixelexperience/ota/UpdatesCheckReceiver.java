e88b7f03c5da87be8c9e954db9a26e392ab49412
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-10-29 23:12:02 -0300

    Don't notify about new update if already downloading/installing
    Change-Id: Iac77369cc79d1630a5fc68ec0e9abd1ba56cc875

--- src/org/pixelexperience/ota/UpdatesCheckReceiver.java
+++ src/org/pixelexperience/ota/UpdatesCheckReceiver.java
@@ -29,8 +29,10 @@ import android.util.Log;
 import androidx.core.app.NotificationCompat;
 
 import org.json.JSONException;
+import org.pixelexperience.ota.controller.ABUpdateInstaller;
 import org.pixelexperience.ota.download.DownloadClient;
 import org.pixelexperience.ota.misc.Utils;
+import org.pixelexperience.ota.model.UpdateStatus;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,8 +48,12 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
 
     private static final String NEW_UPDATES_NOTIFICATION_CHANNEL =
             "new_updates_notification_channel";
+    public static final int NOTIFICATION_ID = 20;
 
     private static void showNotification(Context context) {
+        if (ABUpdateInstaller.needsReboot() || Utils.getPersistentStatus(context) != UpdateStatus.Persistent.UNKNOWN) {
+            return;
+        }
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationChannel notificationChannel = new NotificationChannel(
@@ -64,7 +70,7 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
         notificationBuilder.setContentTitle(context.getString(R.string.update_found_notification));
         notificationBuilder.setAutoCancel(true);
         notificationManager.createNotificationChannel(notificationChannel);
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
     private static PendingIntent getRepeatingUpdatesCheckIntent(Context context) {
