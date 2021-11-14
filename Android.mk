41b2c0eea3428b9bf0ec57a4222da5c243500513
Michael Bestas <mkbestas@lineageos.org>
2021-11-14 21:34:25 +0200

    Updater: Move to /system_ext
    Change-Id: Ib781432398da8550eeb75b9d40dab191ce9f81c0

--- Android.mk
+++ Android.mk
@@ -27,6 +27,7 @@ LOCAL_PACKAGE_NAME := Updates
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_REQUIRED_MODULES := privapp_whitelist_org.pixelexperience.ota.xml
@@ -37,7 +38,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp_whitelist_org.pixelexperience.ota.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
