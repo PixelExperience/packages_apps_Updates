c4453d10263802d2844429001e2484ef1b893ff6
jhenrique09 <jhsv09@gmail.com>
2022-11-03 14:32:20 -0300

    Update api endpoint
    Change-Id: I9c0f1f373c6926ecce7585342dd1fa3d95077afb

--- app/src/main/java/org/pixelexperience/ota/misc/Constants.java
+++ app/src/main/java/org/pixelexperience/ota/misc/Constants.java
@@ -30,8 +30,8 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
+    static final String OTA_URL = "https://api.pixelexperience.org/ota/%s/%s";
+    static final String OTA_CI_URL = "https://api.pixelexperience.org/ota_ci/%s/%s";
     static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
     static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
